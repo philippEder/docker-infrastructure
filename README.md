269739ea678aeba811931c95fbcdf4de95030053
= <p.eder92@gmx.at>
2020-10-15 21:40:10 +0200

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+Repository for my infrastructure studies
---
+++ gitlab/gitlab-manage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "---REMOVE OLD GITLAB CONTAINER IF EXISTS ----"
+
+docker rm -f gitlab || true
+
+
+echo "----RUNNING GITLAB DOCKER IMAGE----"
+
+sudo docker run --detach \
+  --hostname gitlab.peder.com \
+  --publish 1443:443 --publish 180:80 --publish 122:22 \
+  --name gitlab \
+  --restart always \
+  --volume $GITLAB_HOME/config:/etc/gitlab \
+  --volume $GITLAB_HOME/logs:/var/log/gitlab \
+  --volume $GITLAB_HOME/data:/var/opt/gitlab \
+  gitlab/gitlab-ee:latest
+
+echo "----DONE RUNNING GITLAB DOCKER IMAGE----"
